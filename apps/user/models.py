1f3363af0190177e82aee582e638f260201bbae4
Mohammad Hoseyn Mollatayfeh <mh.molataifeh@zhinogroup.com>
2024-09-03 20:22:46 +0330

    feat: Add updated_at and created_at to user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -7,6 +7,8 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=11, null=True, blank=True)
     is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.first_name} - {self.last_name} - {self.email}"
