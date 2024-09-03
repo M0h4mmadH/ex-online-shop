df9007799d8b504ff845effefd94e4c35961f2af
Mohammad Hoseyn Mollatayfeh <mh.molataifeh@zhinogroup.com>
2024-09-03 20:27:56 +0330

    feat: Add Moc service and selectors

---
+++ apps/shop/selectors.py
@@ -0,0 +1,5 @@
+from apps.shop.models import *
+
+
+def generate_purchase_order_factor(order: Order):
+    pass
---
+++ apps/shop/services.py
@@ -0,0 +1,7 @@
+
+
+def send_sms(phone_number: str, message: str):
+    print('send sms successfully')
+
+def send_email(email: str, message: str):
+    print('send email successfully')
